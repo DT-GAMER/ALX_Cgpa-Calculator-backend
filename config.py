fd3cdff36e7ee2e91815f3c3de86b4634e091dba
Abakpa Dominic <abakpad82@gmail.com>
2023-06-08 07:24:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('bvUx^L7@3L#t^Z4n')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@hostname:port/database_name'
+    SQLALCHEMY_DATABASE_URI = 'postgres://gradeccalculator-main-db-0b2e170f6fc37da17:fSjjF9k34XAC1RgVJBd8xvEezU7yqd@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/gradeccalculator-main-db-0b2e170f6fc37da17'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 

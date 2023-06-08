16810fb61c3d0d10b14bb908ab7d1979ff95b891
Abakpa Dominic <abakpad82@gmail.com>
2023-06-08 02:48:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@hostname:port/database_name'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
